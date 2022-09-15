858abba0f1c0ea680ef5a47231fc1a1d251fb30a
krishna <0186359@schoolsnet.act.edu.au>
2022-09-15 11:52:28 +1000

    enemy
    enemy vision cone

---
+++ Enemy.gd
@@ -0,0 +1,38 @@
+extends KinematicBody
+
+var space_state
+var target
+
+func _ready():
+	space_state = get_world().direct_space_state
+
+func _process(delta):
+	if target:
+		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
+		if result.collider.is_in_group("Player"):
+			look_at(target.global_transform.origin, Vector3.UP)
+			set_color_red()
+		else:
+			set_color_blue()
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+		target = body
+		print(body.name + "entered")
+		set_color_red()
+
+
+func _on_Area_body_exited(body):
+	if body.is_in_group("Player"):
+		target = null
+		print(body.name + "exited")
+		set_color_blue()
+
+func move_to_target(Delta):
+	
+
+func set_color_red():
+	$MeshInstance.get_surface_material(0).set_albedo(Color(1, 0, 0))
+	
+func set_color_blue():
+	$MeshInstance.get_surface_material(0).set_albedo(Color(0, 0, 1))
